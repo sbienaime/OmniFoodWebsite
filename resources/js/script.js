1efcefeb407321ab9614dbc715568bcb7ed1c260
Nikola901 <stevebienaime@gmail.com>
2019-03-12 03:30:06 -0400

    sticky navigation plus custom header

---
+++ resources/js/script.js
@@ -0,0 +1,53 @@
+$(document).ready (function(){
+ /*                 
+                  
+   $('h1').click(function(){
+           $(this).css('background-color','#ff0000');
+                  
+                                                     })
+    
+    var waypoints = $('#handler-first').waypoint(function(direction) {
+  notify(this.element.id + ' hit 25% from top of window') 
+}, {
+  offset: '25%'
+}) 
+                  
+                  })
+                  
+                  */
+    $('.js--section-features').waypoint(function(direction){
+                                         
+         if (direction == "down")    {
+        
+        
+        $('nav').addClass('sticky');
+        
+        
+      }                            
+                                         
+    else{
+        
+        
+        $('nav').removeClass('sticky');
+        
+    }
+        
+        
+        
+        
+        
+    }, {
+  offset: '60px;'
+})                            
+                                         
+                                         
+                                         
+   
+    
+    
+    
+    
+    
+    
+    
+                  });
